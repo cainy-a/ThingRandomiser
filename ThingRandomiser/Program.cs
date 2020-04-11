8c182d0de376939a0c0485d255e75bfa322733ae
cainy-a <cainatkinson98@gmail.com>
2020-04-11 14:18:24 +0100

    Initial commit

---
+++ ThingRandomiser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThingRandomiser
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            Console.Write("How many things do you want?");
+            short amount = Convert.ToInt16(Console.ReadLine());
+            List<string> things = new List<string>(amount);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write($"Enter Thing {(i + 1).ToString()}:");
+                things.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine($"I choose: \"{things[rand.Next(things.Count)]}\" as my random pick.");
+        }
+    }
+}
